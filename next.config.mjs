740f6422a8ca403e2163769c3b731118d17fa1b1
apinat25 <apinutaunsoda@gmail.com>
2026-05-12 16:52:11 +0700

    Fix: allow all remote image hosts

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "**" },
     ],
   },
 };
